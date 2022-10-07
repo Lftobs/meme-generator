6415291d1111a58fffb1be4fa57699810c48d6bc
tobsdev12 <tobbytobs1@gmail.com>
2022-10-07 12:31:42 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [darkm, setdarkm] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkm ? 'dark App': 'light App'}  >
+        <button onClick={() => setdarkm(!darkm)} >change</button>
     </div>
+    // <div className="App">
+    // //   <header className="App-header">
+    // //m     <img src={logo} className="App-logo" alt="logo" />
+    // //     <p>
+    // //       Hi, im tobs and i absolutely don't know what im doing next.
+    // //     </p>
+    // //     <a// 
+    // //       className="App-link"
+    // //       href="https://reactjs.org"
+    // //       target="_blank"
+    // //       rel="noopener noreferrer"
+    // //     >
+    // //       Learn React
+    // //     </a>
+    // //   </header>
+    // //   <Random />
+    // </div>
   );
 }
 
---
+++ src/Random.js
@@ -0,0 +1,5 @@
+export default function Random() {
+    return (
+        <h2> this is a footer </h2>
+    )
+}
